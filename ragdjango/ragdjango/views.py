e095d2218f8e8a5392615aa895dc5c0d9480bd72
Ganesh Patil <135629689+ganesh0706patil@users.noreply.github.com>
2025-05-16 00:08:06 +0530

    Update views.py

--- ragdjango/ragdjango/views.py
+++ ragdjango/ragdjango/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
-def home(request):
-    return render(request, 'index.html')
+urlpatterns = [
+    path('', include('rag.urls')),  # Your app's URLs
+]
 
-def about(request):
-    return render(request, 'about.html')
-
-def contact(request):
-    return render(request, 'contact.html')
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
